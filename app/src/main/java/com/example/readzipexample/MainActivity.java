2d737fea1487c946cd5c7105de77f808a8169929
Huong NV <huongnv@tohsoft.com>
2019-08-07 18:41:55 +0700

    a

--- app/src/main/java/com/example/readzipexample/MainActivity.java
+++ app/src/main/java/com/example/readzipexample/MainActivity.java
@@ -2,28 +2,34 @@ package com.example.readzipexample;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.content.res.XmlResourceParser;
 import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.MediaStore;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.util.Xml;
 import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.blankj.utilcode.util.LogUtils;
 import com.blankj.utilcode.util.UriUtils;
 
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
 import java.io.File;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int PICKFILE_REQUEST_CODE = 1111;
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +50,100 @@ public class MainActivity extends AppCompatActivity {
         startActivityForResult(intent, PICKFILE_REQUEST_CODE);
     }
 
+    protected static CharSequence getTextFromResourceOrText(
+            Context context, AttributeSet attrs, String attributeName) {
+        final int stringResId =
+                attrs.getAttributeResourceValue(null, attributeName, 0);
+        if (stringResId != 0) {
+            return context.getResources().getText(stringResId);
+        } else {
+            return attrs.getAttributeValue(null, attributeName);
+        }
+    }
+
+    private int getApiVersion(Context packContext) {
+        try {
+            final Resources resources = packContext.getResources();
+            final int identifier =
+                    resources.getIdentifier(
+                            "anysoftkeyboard_api_version_code",
+                            "integer",
+                            packContext.getPackageName());
+            if (identifier == 0) return 0;
+
+            return resources.getInteger(identifier);
+        } catch (Exception e) {
+            Log.w(
+                    TAG,
+                    "Failed to load api-version for package %s" +
+                            packContext.getPackageName());
+            return 0;
+        }
+    }
+
+    private void createAddOnFromXmlAttributes(AttributeSet attrs, Context packContext) {
+        final CharSequence prefId =
+                getTextFromResourceOrText(packContext, attrs, "id");
+        final CharSequence name =
+                getTextFromResourceOrText(packContext, attrs, "nameResId");
+
+//        if (!mDevAddOnsIncluded
+//                && attrs.getAttributeBooleanValue(null, XML_DEV_ADD_ON_ATTRIBUTE, false)) {
+//            Logger.w(
+//                    mTag,
+//                    "Discarding add-on %s (name %s) since it is marked as DEV addon, and we're not a TESTING_BUILD build.",
+//                    prefId,
+//                    name);
+//            return null;
+//        }
+
+        final int apiVersion = getApiVersion(packContext);
+        final boolean isHidden =
+                attrs.getAttributeBooleanValue(null, "hidden", false);
+        final CharSequence description =
+                getTextFromResourceOrText(packContext, attrs, "description");
+
+        final int sortIndex = attrs.getAttributeUnsignedIntValue(null, "index", 1);
+
+        // asserting
+        if (TextUtils.isEmpty(prefId) || TextUtils.isEmpty(name)) {
+            Log.e(
+                    TAG,
+                    "External add-on does not include all mandatory details! Will not create add-on.");
+            return;
+        } else {
+            Log.d(TAG, "External addon details: prefId:" + prefId + " name:" + name);
+
+        }
+    }
+
+    public void parseAddOnsFromXml(Context packContext, XmlPullParser xml) {
+        try {
+            int event;
+            boolean inRoot = false;
+            while ((event = xml.next()) != XmlPullParser.END_DOCUMENT) {
+                final String tag = xml.getName();
+                if (event == XmlPullParser.START_TAG) {
+                    if ("KeyboardThemes".equals(tag)) {
+                        inRoot = true;
+                    } else if (inRoot && "KeyboardTheme".equals(tag)) {
+                        final AttributeSet attrs = Xml.asAttributeSet(xml);
+                        createAddOnFromXmlAttributes(attrs, packContext);
+                    }
+                } else if (event == XmlPullParser.END_TAG && "KeyboardThemes".equals(tag)) {
+                    inRoot = false;
+                    break;
+                }
+            }
+        } catch (final IOException e) {
+            Log.e(TAG, "IO error:" + e);
+            e.printStackTrace();
+        } catch (final XmlPullParserException e) {
+            Log.e(TAG, "Parse error:" + e);
+            e.printStackTrace();
+        }
+
+    }
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (data != null && requestCode == PICKFILE_REQUEST_CODE) {
             Uri uri = data.getData();
@@ -51,7 +151,7 @@ public class MainActivity extends AppCompatActivity {
             File file = UriUtils.uri2File(uri);
 
             if (file.exists()) {
-                getResourceInThemePlugin(this, file, "theme");
+                getResourceInThemePlugin(this, file, "three_d_themes");
             }
         }
         super.onActivityResult(requestCode, resultCode, data);
@@ -68,49 +168,15 @@ public class MainActivity extends AppCompatActivity {
         return s;
     }
 
-    public static @Nullable
+    public @Nullable
     Drawable getResourceInThemePlugin(Context context, File file, String identifyResourceName) {
+        Resources resources = ApkPluginUtil.getApkResources(context, file.getAbsolutePath());
+        ReflectResource reflectResource = new ReflectResource(resources, "com.anysoftkeyboard");
 
-        ReflectResource reflectResource = new ReflectResource(ApkPluginUtil.getApkResources(context, file.getAbsolutePath()), "theme");
-
-        if (reflectResource != null) {
-
-            try {
-                XmlResourceParser xmlResourceParser = reflectResource.getResApkXml("three_d_themes");
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
+        XmlResourceParser xmlResourceParser = reflectResource.getResApkXml(identifyResourceName);
+        parseAddOnsFromXml(context, xmlResourceParser);
+        Log.e(TAG, xmlResourceParser.toString());
 //            Drawable drawable = reflectResource.getResApkDrawable(identifyResourceName);
-            return null;
-        } else {
-            // không có trong resource apk, phải lấy trong data folder.
-            File rootFolder = new File(Environment.getExternalStorageDirectory(), "Download");
-            if (!rootFolder.exists()) {
-                LogUtils.e("Resource not found!");
-            } else {
-                try {
-//                        String filePath = rootFolder.getPath() + File.separator
-//                                + theme + File.separator
-//                                + "res" + File.separator
-//                                + "drawabletest" + File.separator
-//                                + identifyResourceName + ".png";
-
-
-                    ReflectResource reflectResource1 = new ReflectResource(ApkPluginUtil.getApkResources(context, file.getAbsolutePath()), "theme");
-
-                    Drawable drawable = reflectResource1.getResApkDrawable(identifyResourceName);
-
-                    Bitmap bitmap = ((BitmapDrawable) drawable).getBitmap();
-
-//                        Bitmap b = BitmapFactory.decodeFile(str);
-                    return new FastBitmapDrawable(bitmap);
-                } catch (Exception e) {
-                    LogUtils.e(e);
-                }
-
-            }
-        }
         return null;
     }
 }
